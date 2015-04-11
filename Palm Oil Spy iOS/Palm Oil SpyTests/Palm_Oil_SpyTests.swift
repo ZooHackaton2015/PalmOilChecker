338596c0674a4d9ea46cd2f4b108e628e9ade386
Vladimír Bělohradský <vladimir@belohradsky.cz>
2015-04-11 16:13:20 +0200

    Init commit of iOS client

---
+++ Palm Oil Spy iOS/Palm Oil SpyTests/Palm_Oil_SpyTests.swift
@@ -0,0 +1,36 @@
+//
+//  Palm_Oil_SpyTests.swift
+//  Palm Oil SpyTests
+//
+//  Created by Vladimír Bělohradský on 11/04/15.
+//  Copyright (c) 2015 GDGSCL. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class Palm_Oil_SpyTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
