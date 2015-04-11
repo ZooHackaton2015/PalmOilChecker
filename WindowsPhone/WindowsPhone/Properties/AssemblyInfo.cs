5ad67d5777263196f0376db43876927062362f2f
RadarCZ <saber.leonard@gmail.com>
2015-04-11 17:05:19 +0200

    Reverting back to Silverlight

--- WindowsPhone/WindowsPhone/Properties/AssemblyInfo.cs
+++ WindowsPhone/WindowsPhone/Properties/AssemblyInfo.cs
@@ -1,10 +1,11 @@
 ﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
+using System.Resources;
 
-// Obecné informace o sestavení jsou řízeny prostřednictvím následující 
-// sadu atributů. Změnou hodnot těchto atributů se upraví informace
-// přidružené k sestavení.
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
 [assembly: AssemblyTitle("WindowsPhone")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
@@ -14,16 +15,23 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Informace o verzi sestavení se skládá z následujících čtyř hodnot:
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("66540887-7ed1-4eb7-80f3-225e423cf4ae")]
+
+// Version information for an assembly consists of the following four values:
 //
-//      Hlavní verze
-//      Dílčí verze 
-//      Číslo sestavení
-//      Revize
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
 //
-// Můžete zadat všechny hodnoty nebo nastavit výchozí očíslování sestavení a revize 
-// použitím znaku '*' jak je ukázáno dále:
-// [sestavení: AssemblyVersion("1.0.*")]
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
-[assembly: ComVisible(false)]
+[assembly: NeutralResourcesLanguageAttribute("cs-CZ")]
