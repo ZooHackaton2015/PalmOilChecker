28668e0ab7f9102ce3e396e5089841a46d963d60
RadarCZ <saber.leonard@gmail.com>
2015-04-11 18:55:09 +0200

    Made functions for Barcode reading

--- WindowsPhone/WindowsPhone/MainPage.xaml.cs
+++ WindowsPhone/WindowsPhone/MainPage.xaml.cs
@@ -8,11 +8,25 @@ using System.Windows.Navigation;
 using Microsoft.Phone.Controls;
 using Microsoft.Phone.Shell;
 using WindowsPhone.Resources;
+using Microsoft.Devices;
+using System.IO;
+using System.IO.IsolatedStorage;
+using Microsoft.Xna.Framework.Media;
+using ZXing;
+using System.Windows.Threading;
+using System.Windows.Media.Imaging;
+using System.Windows.Input;
 
 namespace WindowsPhone
 {
     public partial class MainPage : PhoneApplicationPage
     {
+
+        private PhotoCamera _phoneCamera;
+        private IBarcodeReader _barcodeReader;
+        private DispatcherTimer _scanTimer;
+        private WriteableBitmap _previewBuffer;
+
         // Constructor
         public MainPage()
         {
@@ -20,6 +34,116 @@ namespace WindowsPhone
 
             // Sample code to localize the ApplicationBar
             //BuildLocalizedApplicationBar();
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            _phoneCamera = new PhotoCamera();
+            _phoneCamera.Initialized += cam_Initialized;
+            _phoneCamera.AutoFocusCompleted += _phoneCamera_AutoFocusCompleted;
+            CameraButtons.ShutterKeyHalfPressed += CameraButtons_ShutterKeyHalfPressed;
+
+            viewfinderBrush.SetSource(_phoneCamera);
+
+            _scanTimer = new DispatcherTimer();
+            _scanTimer.Interval = TimeSpan.FromMilliseconds(250);
+            _scanTimer.Tick += (o, arg) => ScanForBarcode();
+
+            viewfinderCanvas.Tap += new EventHandler<GestureEventArgs>(focus_Tapped);
+
+            base.OnNavigatedTo(e);
+        }
+
+        void _phoneCamera_AutoFocusCompleted(object sender, CameraOperationCompletedEventArgs e)
+        {
+            Deployment.Current.Dispatcher.BeginInvoke(delegate()
+            {
+                focusBrackets.Visibility = Visibility.Collapsed;
+            });
+        }
+
+        void focus_Tapped(object sender, GestureEventArgs e)
+        {
+            if (_phoneCamera != null)
+            {
+                if (_phoneCamera.IsFocusAtPointSupported == true)
+                {
+                    Point tapLocation = e.GetPosition(viewfinderCanvas);
+                    focusBrackets.SetValue(Canvas.LeftProperty, tapLocation.X - 30);
+                    focusBrackets.SetValue(Canvas.TopProperty, tapLocation.Y - 28);
+
+                    double focusXPercentage = tapLocation.X / viewfinderCanvas.ActualWidth;
+                    double focusYPercentage = tapLocation.Y / viewfinderCanvas.ActualHeight;
+
+                    focusBrackets.Visibility = Visibility.Visible;
+                    _phoneCamera.FocusAtPoint(focusXPercentage, focusYPercentage);
+                }
+            }
+        }
+
+        void CameraButtons_ShutterKeyHalfPressed(object sender, EventArgs e)
+        {
+            _phoneCamera.Focus();
+        }
+
+        protected override void onNavigatingFrom(NavigatingCancelEventArgs e)
+        {
+            _scanTimer.Stop();
+
+            if (_phoneCamera != null)
+            {
+                _phoneCamera.Dispose();
+                _phoneCamera.Initialized -= cam_Initialized;
+                CameraButtons.ShutterKeyHalfPressed -= CameraButtons_ShutterKeyHalfPressed;
+            }
+        }
+
+        void cam_Initialized(object sender, CameraOperationCompletedEventArgs e)
+        {
+            if (e.Succeeded)
+            {
+                this.Dispatcher.BeginInvoke(delegate()
+                {
+                    _phoneCamera.FlashMode = FlashMode.Off;
+                    _previewBuffer = new WriteableBitmap((int)_phoneCamera.PreviewResolution.Width, (int)_phoneCamera.PreviewResolution.Height);
+
+                    _barcodeReader = new BarcodeReader();
+                    var SupportedBarcodeFormats = new List<BarcodeFormat>();
+                    SupportedBarcodeFormats.Add(BarcodeFormat.EAN_13);
+                    SupportedBarcodeFormats.Add(BarcodeFormat.EAN_8);
+                    _barcodeReader.Options.PossibleFormats = SupportedBarcodeFormats;
+
+                    _barcodeReader.Options.TryHarder = true;
+
+                    _barcodeReader.ResultFound +=_barcodeReader_ResultFound;
+                    _scanTimer.Start();
+                });
+            }
+            else
+            {
+                Dispatcher.BeginInvoke(() =>
+                {
+                    MessageBox.Show("Nebylo možné spustit fotoaparát.");
+                });
+            }
+        }
+
+        void _barcodeReader_ResultFound(Result obj)
+        {
+            if (!obj.Text.Equals(tbBarcodeData.Text))
+            {
+                VibrateController.Default.Start(TimeSpan.FromMilliseconds(100));
+                tbBarcodeType.Text = obj.BarcodeFormat.ToString();
+                tbBarcodeData.Text = obj.Text;
+            }
+        }
+
+        private void ScanForBarcode()
+        {
+            _phoneCamera.GetPreviewBufferArgb32(_previewBuffer.Pixels);
+            _previewBuffer.Invalidate();
+
+            _barcodeReader.Decode(_previewBuffer);
         }
 
         // Sample code for building a localized ApplicationBar
