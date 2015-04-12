8bf3af164d52cb2141a4a13f12da52ab15f74374
RadarCZ <saber.leonard@gmail.com>
2015-04-12 10:22:46 +0200

    Added barcode scanning library

--- Android/PalmOilScan/app/src/main/java/cz/gdgscl/palmoilscan/MainActivity.java
+++ Android/PalmOilScan/app/src/main/java/cz/gdgscl/palmoilscan/MainActivity.java
@@ -1,19 +1,43 @@
 package cz.gdgscl.palmoilscan;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.app.Activity;
+import android.content.Intent;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
 
 
-public class MainActivity extends ActionBarActivity {
+public abstract class MainActivity extends Activity implements OnClickListener {
+
+    private Button scanBtn;
+    private TextView formatTxt, contentTxt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        scanBtn = (Button)findViewById(R.id.scan_button);
+        formatTxt = (TextView)findViewById(R.id.scan_format);
+        contentTxt=(TextView)findViewById(R.id.scan_content);
+
+        scanBtn.setOnClickListener(this);
     }
 
+    public void onClick(View v){
+        if(v.getId()==R.id.scan_button){
+            IntentIntegrator scanIntegrator = new IntentIntegrator(this);
+            scanIntegrator.initiateScan();
+        }
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
