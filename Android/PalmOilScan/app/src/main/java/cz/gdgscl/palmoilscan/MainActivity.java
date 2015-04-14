62a90858e5214c8af6e84bc525549e65c0faf424
RadarCZ <saber.leonard@gmail.com>
2015-04-14 10:24:02 +0200

    Written own library and edited other files to work with it

--- Android/PalmOilScan/app/src/main/java/cz/gdgscl/palmoilscan/MainActivity.java
+++ Android/PalmOilScan/app/src/main/java/cz/gdgscl/palmoilscan/MainActivity.java
@@ -1,75 +1,60 @@
 package cz.gdgscl.palmoilscan;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.hardware.Camera;
+import android.hardware.Camera.PictureCallback;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.app.Activity;
-import android.content.Intent;
+import android.view.Menu;
 import android.view.View;
-import android.view.View.OnClickListener;
-import android.widget.Button;
-import android.widget.TextView;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
 import android.widget.Toast;
 
-import com.google.zxing.integration.android.IntentIntegrator;
-import com.google.zxing.integration.android.IntentResult;
+public class MainActivity extends Activity {
 
-public class MainActivity extends Activity implements OnClickListener {
+    private Camera cameraObject;
+    private ShowCamera showCamera;
+    private ImageView pic;
+    public static Camera isCameraAvailiable(){
+        Camera object = null;
+        try {
+            object = Camera.open();
+        }
+        catch (Exception e){
+        }
+        return object;
+    }
 
-    private Button scanBtn;
-    private TextView formatTxt, contentTxt;
+    private PictureCallback capturedIt = new PictureCallback() {
+
+        @Override
+        public void onPictureTaken(byte[] data, Camera camera) {
+
+            Bitmap bitmap = BitmapFactory.decodeByteArray(data , 0, data .length);
+            if(bitmap==null){
+                Toast.makeText(getApplicationContext(), "not taken", Toast.LENGTH_SHORT).show();
+            }
+            else
+            {
+                Toast.makeText(getApplicationContext(), "taken", Toast.LENGTH_SHORT).show();
+            }
+            cameraObject.release();
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        scanBtn = (Button)findViewById(R.id.scan_button);
-        formatTxt = (TextView)findViewById(R.id.scan_format);
-        contentTxt=(TextView)findViewById(R.id.scan_content);
-
-        scanBtn.setOnClickListener(this);
+        pic = (ImageView)findViewById(R.id.imageView1);
+        cameraObject = isCameraAvailiable();
+        showCamera = new ShowCamera(this, cameraObject);
+        FrameLayout preview = (FrameLayout) findViewById(R.id.camera_preview);
+        preview.addView(showCamera);
     }
-
-    public void onClick(View v){
-        if(v.getId()==R.id.scan_button){
-            IntentIntegrator scanIntegrator = new IntentIntegrator(this);
-            scanIntegrator.initiateScan(IntentIntegrator.PRODUCT_CODE_TYPES);
-        }
-    }
-
-    public void onActivityResult(int requestCode, int resultCode, Intent intent){
-        IntentResult scanningResult = IntentIntegrator.parseActivityResult(requestCode, resultCode, intent);
-        if(scanningResult!=null){
-            String scanContent = scanningResult.getContents();
-            String scanFormat = scanningResult.getFormatName();
-            formatTxt.setText("FORMAT: " + scanFormat);
-            contentTxt.setText("CONTENT: " + scanContent);
-        } else {
-            Toast toast = Toast.makeText(getApplicationContext(), "No scan data received!", Toast.LENGTH_SHORT);
-            toast.show();
-        }
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+    public void snapIt(View view){
+        cameraObject.takePicture(null, null, capturedIt);
     }
 }
