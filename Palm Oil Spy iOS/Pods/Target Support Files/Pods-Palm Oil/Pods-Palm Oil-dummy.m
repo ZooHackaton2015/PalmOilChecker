c93c9461ff9669a015b9c3fb2655e56197e8d723
Vladimír Bělohradský <vladimir.belohradsky@gmail.com>
2015-11-24 00:24:30 +0100

    Enable Cocoa Pods
    - add Fabric
    - add Answers
    - add Crashlytics

---
+++ Palm Oil Spy iOS/Pods/Target Support Files/Pods-Palm Oil/Pods-Palm Oil-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Palm_Oil : NSObject
+@end
+@implementation PodsDummy_Pods_Palm_Oil
+@end
---
+++ Palm Oil Spy iOS/Pods/Target Support Files/Pods-Palm Oil/Pods-Palm Oil-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_Palm_OilVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_Palm_OilVersionString[];
+
